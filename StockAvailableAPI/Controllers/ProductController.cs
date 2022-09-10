fd72eeecf667bb69e7eb0a09f6d6490cfc1c70ed
natan5000 <natan823@hotmail.com>
2022-09-10 15:34:18 -0600

    Model, controller, migraciones tabla productos y conf de ef

---
+++ StockAvailableAPI/Controllers/ProductController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StockAvailableAPI.Data;
+using StockAvailableAPI.Models;
+
+namespace StockAvailableAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ProductController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetAll()
+        {
+            return Ok(await _context.products.ToListAsync());
+        }
+
+        [HttpGet("code")]
+        public IActionResult GetCode(string code)
+        {
+            try 
+            {
+                var prod = _context.products.ToList();
+                if(code != null)
+                {
+                    prod = prod.Where(x => x.code.ToLower().IndexOf(code) > -1).ToList();
+                }
+                return Ok(prod);
+            }
+            catch
+            {
+                return BadRequest("Product not found!");
+            }
+        }
+
+       [HttpPost]
+       public async Task<ActionResult<List<Product>>> AddProduct(Product product)
+        {
+            _context.products.Add(product);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.products.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Product>>> UpdateProduct(Product request)
+        {
+            var dbProduct = await _context.products.FindAsync(request Id);
+            if (dbProduct == null)
+                return BadRequest("Product not found!");
+
+            dbProduct.code = request.code;
+            dbProduct.name = request.name;
+            dbProduct.active = request.active;
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.products.ToListAsync());
+        }
+
+    }
+}
---
+++ StockAvailableAPI/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StockAvailableAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Models.Product> products { get; set; }
+    }
+}
