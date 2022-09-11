ba1ed1a05ae28b2c552aa51b80caecd07a66e488
natan5000 <natan823@hotmail.com>
2022-09-10 21:57:32 -0600

    modelos, controlador, migracion para tabla operations

---
+++ StockAvailableAPI/Models/Operation.cs
@@ -0,0 +1,12 @@
+﻿namespace StockAvailableAPI.Models
+{
+    public class Operation
+    {
+        public int Id { get; set; }
+        public string codeBox { get; set; }
+        public int quantity { get; set; }
+        public DateTime date { get; set; }
+        public TypeOperation TypeOperation { get; set; }
+        public int typeOperationID { get; set; }
+    }
+}
