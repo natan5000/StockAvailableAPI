f6447397a534aae084f94f1c507025a4101d427b
natan5000 <natan823@hotmail.com>
2022-09-12 19:46:05 -0600

    reportes y cambios en appsettings

---
+++ StockAvailableAPI/Migrations/20220911045103_agregandoColumnaType.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StockAvailableAPI.Migrations
+{
+    public partial class agregandoColumnaType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "type",
+                table: "typeOperations",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "type",
+                table: "typeOperations");
+        }
+    }
+}
